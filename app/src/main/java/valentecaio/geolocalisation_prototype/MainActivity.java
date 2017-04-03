fea17dbbc80e9a8fa36a9be77f9270bfd029c77f
Caio Valente <caio-valente@hotmail.com>
2017-04-03 11:43:04 +0200

    app getting location

--- app/src/main/java/valentecaio/geolocalisation_prototype/MainActivity.java
+++ app/src/main/java/valentecaio/geolocalisation_prototype/MainActivity.java
@@ -1,13 +1,22 @@
 package valentecaio.geolocalisation_prototype;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +27,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // request permission to user
+        if ( ContextCompat.checkSelfPermission( this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+            int MIN_DISTANCE_CHANGE_FOR_UPDATES = 0;
+            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES  );
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                Log.v("Debug", "onClick");
+                if (displayGpsStatus()) {
+                    Context context = getApplicationContext();
+                    MyLocationListener locationListener = new MyLocationListener(context);
+
+                    // check permission
+                    if ( Build.VERSION.SDK_INT >= 23 &&
+                            ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                            ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        Log.v("Debug", "returning with SDK version " + Build.VERSION.SDK_INT);
+                        return  ;
+                    }
+
+                    locationManager.requestLocationUpdates(LocationManager
+                            .GPS_PROVIDER, 5000, 10,locationListener);
+
+                } else {
+                    Log.v("Debug", "Gps Status!! Your GPS is: OFF");
+                }
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
+
     }
 
     @Override
@@ -49,4 +91,15 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /*----Method to Check GPS is enable or disable ----- */
+    private Boolean displayGpsStatus() {
+        ContentResolver contentResolver = getBaseContext()
+                .getContentResolver();
+        boolean gpsStatus = Settings.Secure
+                .isLocationProviderEnabled(contentResolver,
+                        LocationManager.GPS_PROVIDER);
+        return gpsStatus;
+    }
+
 }
---
+++ app/src/main/java/valentecaio/geolocalisation_prototype/MyLocationListener.java
@@ -0,0 +1,65 @@
+package valentecaio.geolocalisation_prototype; /**
+ * Created by caio on 03/04/2017.
+ */
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+/*----------Listener class to get coordinates ------------- */
+public class MyLocationListener implements LocationListener {
+
+    private Context context;
+
+    public MyLocationListener(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        String longitude = "Longitude: " +loc.getLongitude();
+        Log.v("Debug", longitude);
+        String latitude = "Latitude: " +loc.getLatitude();
+        Log.v("Debug", latitude);
+
+    /*----------to get City-Name from coordinates ------------- */
+        String cityName=null;
+        Geocoder gcd = new Geocoder(context,
+                Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = gcd.getFromLocation(loc.getLatitude(), loc
+                    .getLongitude(), 1);
+            if (addresses.size() > 0)
+                cityName=addresses.get(0).getLocality();
+                Log.v("Debug", "My Currrent City is: " + cityName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStatusChanged(String provider,
+                                int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+}
