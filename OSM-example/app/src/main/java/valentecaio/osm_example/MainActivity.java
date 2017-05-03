5cd99b9d7db7bc5b85e7f133d5873b83b0382a74
Caio Valente <caio-valente@hotmail.com>
2017-05-03 10:12:35 +0200

    organize imports

--- OSM-example/app/src/main/java/valentecaio/osm_example/MainActivity.java
+++ OSM-example/app/src/main/java/valentecaio/osm_example/MainActivity.java
@@ -7,11 +7,11 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 
 import org.osmdroid.bonuspack.overlays.Marker;
