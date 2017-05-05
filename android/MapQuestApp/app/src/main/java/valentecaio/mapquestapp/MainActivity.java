823f47d7c48747fe8e85a4782dedfc81388a1eae
Caio Valente <caio-valente@hotmail.com>
2017-05-06 00:54:56 +0200

    first map example (MapQuestApp)

--- android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/MainActivity.java
+++ android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/MainActivity.java
@@ -3,11 +3,47 @@ package valentecaio.mapquestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapquest.mapping.MapQuestAccountManager;
+import com.mapquest.mapping.maps.OnMapReadyCallback;
+import com.mapquest.mapping.maps.MapboxMap;
+import com.mapquest.mapping.maps.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private MapboxMap mMapboxMap;
+    private MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MapQuestAccountManager.start(getApplicationContext());
+
         setContentView(R.layout.activity_main);
+
+        mMapView = (MapView) findViewById(R.id.mapquestMapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mMapboxMap = mapboxMap;
+            }
+        });
     }
+
+    @Override
+    public void onResume()
+    { super.onResume(); mMapView.onResume(); }
+
+    @Override
+    public void onPause()
+    { super.onPause(); mMapView.onPause(); }
+
+    @Override
+    protected void onDestroy()
+    { super.onDestroy(); mMapView.onDestroy(); }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    { super.onSaveInstanceState(outState); mMapView.onSaveInstanceState(outState); }
 }
+
