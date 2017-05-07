23efeaaf69084e129eebf002db9fc4b26e09cea7
Caio Valente <caio-valente@hotmail.com>
2017-05-07 12:30:42 +0200

    add camera to cameraView

--- android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/CameraActivity.java
+++ android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/CameraActivity.java
@@ -1,13 +1,82 @@
 package valentecaio.mapquestapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class CameraActivity extends AppCompatActivity {
+import java.io.IOException;
+
+import static valentecaio.mapquestapp.R.id.camera_view;
+
+public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private Camera mCamera;
+    private SurfaceHolder mSurfaceHolder;
+    private boolean isCameraviewOn = false ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        verifyCameraPermissions();
+
+        SurfaceView surfaceView = (SurfaceView) findViewById(camera_view);
+        mSurfaceHolder = surfaceView.getHolder();
+        mSurfaceHolder.addCallback( this );
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        mCamera = Camera. open();
+        mCamera.setDisplayOrientation( 90);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        if ( isCameraviewOn ) {
+            mCamera.stopPreview();
+            isCameraviewOn = false ;
+        }
+
+        if ( mCamera != null ) {
+            try {
+                mCamera .setPreviewDisplay( mSurfaceHolder);
+                mCamera .startPreview();
+                isCameraviewOn = true ;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null ;
+        isCameraviewOn = false ;
+    }
+
+    public  void verifyCameraPermissions() {
+        String[] PERMISSIONS_STORAGE = {Manifest.permission.CAMERA};
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    1
+            );
+        }
     }
 }
+
