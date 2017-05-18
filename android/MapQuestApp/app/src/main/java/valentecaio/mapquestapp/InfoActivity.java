5fc5f2d06f1a6370c56e5d6c062ed4fc8975e5ff
Caio Valente <caio-valente@hotmail.com>
2017-05-18 03:15:21 +0200

    [ANDROID] add imageView to InfoActivity and imt1.png as example

--- android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/InfoActivity.java
+++ android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/InfoActivity.java
@@ -2,12 +2,19 @@ package valentecaio.mapquestapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class InfoActivity extends AppCompatActivity {
+
+    ImageView photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        photo = (ImageView) findViewById(R.id.imt1);
     }
+
+
 }
