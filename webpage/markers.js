078a2a9437cd28b3c58bb671a1b39678541c093b
Caio Valente <caio-valente@hotmail.com>
2017-05-21 01:55:23 +0200

    [WEB] getting point/balade clicked from global variables

--- webpage/markers.js
+++ webpage/markers.js
@@ -10,10 +10,11 @@ function addmarkers(balade) {
 	map.setCenter(lonLat_center, zoom);
 
 	var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
-	for (i = 0; balade && i < balade.length; i++) {
+	for (i=0; balade && i < balade.length; i++) {
+		console.log(balade[i], balade[i].latitude, balade[i].longitude)
 		var marker = new OpenLayers.Feature.Vector(
-				new OpenLayers.Geometry.Point(balade[i][0], balade[i][1]).transform(epsg4326, projectTo), {
-				description: balade[i][2]
+				new OpenLayers.Geometry.Point(balade[i].latitude, balade[i].longitude).transform(epsg4326, projectTo), {
+				description: balade[i].name
 			}, {
 				externalGraphic: 'image_marker.png',
 				graphicHeight: 30,
@@ -58,11 +59,8 @@ function addmarkers(balade) {
 
 // dinamically add rows to table
 // data must be an iterable object where each entry has a attribute name
-function add_rows(table_id, data) {
+function add_rows(table_id, data, onclick_function) {
 	for (i = 0; i < data.length; i++) {
-		console.log(data[i].name);
-		console.log(data[i].latitude);
-
 		var new_row = document.createElement('div');
 		new_row.className = "btn-group";
 		new_row.style = "width:100%";
@@ -71,7 +69,7 @@ function add_rows(table_id, data) {
 		but1.style = "width:90%;height: 40px; text-align: left; color: black;";
 		but1.className = "btn btn-default";
 		but1.innerHTML = data[i].name;
-		but1.setAttribute('onclick', 'changeMap("' + data[i].name + '")');
+		but1.setAttribute('onclick', onclick_function + "('" + data[i].name + "')");
 
 		var but_edit = document.createElement('button');
 		but_edit.style = "width:10%;height: 40px;";
