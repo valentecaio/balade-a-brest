d8e123e1f03bf70ee286e25d9004ca7a34d44a43
Caio Valente <caio-valente@hotmail.com>
2017-05-21 02:00:27 +0200

    [WEB] add setup_map function to init map

--- webpage/markers.js
+++ webpage/markers.js
@@ -1,3 +1,14 @@
+function setup_map(center, zoom) {
+	map = new OpenLayers.Map("mapdiv");
+	map.addLayer(new OpenLayers.Layer.OSM());
+
+	epsg4326 = new OpenLayers.Projection("EPSG:4326");
+	projectTo = map.getProjectionObject();
+
+	var lonLat_center = new OpenLayers.LonLat(center.lon, center.lat).transform(epsg4326, projectTo);
+	map.setCenter(lonLat_center, zoom);
+}
+
 function addmarkers(balade) {
 	map = new OpenLayers.Map("mapdiv");
 	map.addLayer(new OpenLayers.Layer.OSM());
