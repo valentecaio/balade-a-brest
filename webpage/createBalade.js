3bb88b3bf50a1a1216adda0910d6747c0b259510
valentecaio <caio-valente@hotmail.com>
2017-05-25 23:34:00 +0100

    (WEB) add dinamic balades list in createBalade

--- webpage/createBalade.js
+++ webpage/createBalade.js
@@ -5,15 +5,42 @@ destinations = [];
 // remove repeated values from an array
 // found at https://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
 function arrayUnique(array) {
-    var a = array.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j--, 1);
-        }
-    }
+	var a = array.concat();
+	for (var i = 0; i < a.length; ++i) {
+		for (var j = i + 1; j < a.length; ++j) {
+			if (a[i] === a[j])
+				a.splice(j--, 1);
+		}
+	}
 
-    return a;
+	return a;
+}
+
+function refresh_balades_table() {
+	// get table reference
+	table = document.getElementById('points_list');
+	
+	// delete table rows
+	while (table.firstChild) {
+		table.removeChild(table.firstChild);
+	}
+
+	for (i=0; destinations && i < destinations.length; i++) {
+		// create new row
+		var new_row = document.createElement('div');
+		new_row.className = "btn-group";
+		new_row.style = "width:100%";
+
+		var but1 = document.createElement('button');
+		but1.style = "width:90%;height: 40px; text-align: left; color: black;";
+		but1.className = "btn btn-default";
+		but1.innerHTML = destinations[i].name + ' (' + destinations[i].lon + ', ' + destinations[i].lat + ')';
+
+		new_row.appendChild(but1);
+		
+		// add new row to table
+		table.appendChild(new_row);
+	}
 }
 
 // add points to map (with controller)
@@ -30,9 +57,11 @@ function setup_click_listener() {
 		var clicked_point = feature.attributes.point;
 		console.log(clicked_point)
 		destinations.push(clicked_point);
-		
+
 		// remove destination if already chosen before this event
 		destinations = arrayUnique(destinations);
+
+		refresh_balades_table();
 	}
 
 	map.addControl(controls['selector']);
@@ -41,7 +70,7 @@ function setup_click_listener() {
 
 function main() {
 	points = get_all_points();
-	
+
 	// load map without points
 	map,
 	markersVectorLayer = setup_map(
@@ -49,10 +78,10 @@ function main() {
 				lon: -4.50010299,
 				lat: 48.38423089
 			}, zoom = 14);
-	
+
 	// plot all points on map
 	refresh_markers(map, markersVectorLayer, points);
-	
+
 	// add click listener to markers
 	setup_click_listener();
 }
