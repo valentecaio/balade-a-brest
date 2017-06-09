083f3780f0db8b18231de72b2b3997d98b86e1d8
Caio Valente <caio-valente@hotmail.com>
2017-06-09 04:28:28 +0200

    [web] correction in some old merge of createPoint

--- webpage/createPoint.php
+++ webpage/createPoint.php
@@ -11,78 +11,8 @@
 		<script src="lib/jquery.min.js"></script>
   		<script src="lib/bootstrap.min.js"></script>
 		<script src="validateFormModUser.js"></script>
-		<script>
-			var map,markers,point_clicked;
-			var zoom = 16;
-			var curpos = new Array();
-			
-			var tanguy_tower = [48.383410, -4.496798]
-			
-			var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-			var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-			
-			var cntrposition = new OpenLayers.LonLat(tanguy_tower[1], tanguy_tower[0]).transform( fromProjection, toProjection);
-			
-			function init()
-			{
-				map = new OpenLayers.Map("Map");
-				markers = new OpenLayers.Layer.Markers("Markers");
-				
-				var mapnik = new OpenLayers.Layer.OSM("MAP"); 
-				
-				map.addLayers([mapnik,markers]);
-				map.setCenter(cntrposition, zoom);
-				
-				var click = new OpenLayers.Control.Click();
-				map.addControl(click);
-				click.activate();
-			};
-			
-			
-			OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {               
-				defaultHandlerOptions: {
-					'single': true,
-					'double': false,
-					'pixelTolerance': 0,
-					'stopSingle': false,
-					'stopDouble': false
-				},
-				
-				initialize: function(options) {
-					this.handlerOptions = OpenLayers.Util.extend(
-					{}, this.defaultHandlerOptions
-					);
-					OpenLayers.Control.prototype.initialize.apply(
-					this, arguments
-					);
-					this.handler = new OpenLayers.Handler.Click(
-					this, {
-						'click': this.trigger
-					}, this.handlerOptions
-					);
-				},
-				
-				trigger: function(e) {
-					// get point values
-					var lonlat = map.getLonLatFromPixel(e.xy);
-					point_clicked = new OpenLayers.LonLat(lonlat.lon,lonlat.lat).transform(toProjection,fromProjection);
-					
-					// remove old markers
-					if(map.layers[1]){
-						map.layers[1].clearMarkers()
-					}
-					
-					// add marker to map
-					var new_position = new OpenLayers.LonLat(point_clicked.lon, point_clicked.lat).transform(fromProjection, toProjection);
-					markers.addMarker(new OpenLayers.Marker(new_position));
-					
-					// add point data to form boxes
-					document.getElementById("form_latitude").value = point_clicked.lat
-					document.getElementById("form_longitude").value = point_clicked.lon
-				}
-				
-			});
-		</script>
+		<script src="markers.js"></script>
+		<script src="createPoint.js"></script>
 	</head>
 
 	<?php
@@ -106,7 +36,7 @@
 		</script>
 	<?php } ?>
 	
-	<body onload='init();'>
+	<body onload='main();'>
 		<?php include 'navigationBar.html';	?>
 		<div class="container-fluid">
 			<div class="row">
@@ -145,7 +75,6 @@
    						</div>
    						<div class="col-sm-6 pull-right">
 			    			<button type="submit" class="btn btn-default">Annuler</button>
-			    			<!--<button type="submit" class="btn" data-toggle="modal" data-target="#sendModal">Envoyer</button>-->
 			    			<button type="submit" class="btn">Envoyer</button>
 			    		</div>
   					</form>
@@ -176,7 +105,7 @@
 			    	</div>
 			  	</div>
 				<div class="col-sm-8">
-					<div style="width: 100%; height: 550px"  id="Map" ></div>
+					<div style="width: 100%; height: 550px"  id="mapdiv" ></div>
 				</div>
 			</div>
 		</div>
