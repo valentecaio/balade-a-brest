da2e1a55e615b12047978f749640979fed5ef277
caio valente <caio-valente@hotmail.com>
2017-06-19 13:40:55 +0200

    [android] add AppFileManager with balade and points write/read functions

---
+++ android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/Balade.java
@@ -0,0 +1,51 @@
+package valentecaio.mapquestapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by caio on 18/6/2017.
+ */
+
+public class Balade {
+    private String id;
+    private String name;
+    private String theme;
+    private ArrayList points = new ArrayList();
+
+    public Balade(String id, String name, String theme) {
+        this.id = id;
+        this.name = name;
+        this.theme = theme;
+    }
+
+    public Balade(String name, String theme) {
+        this.name = name;
+        this.theme = theme;
+    }
+
+    public ArrayList getPoints() {
+        return points;
+    }
+
+    public void addPoint(Point p) {
+        this.points.add(p);
+    }
+
+    public void addPoint(String id) {
+        // TODO: this function may find the point using its id and add the Point to this.points
+        // Point p = fundById(id);
+        // addPoint(p);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+}
--- android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/Point.java
+++ android/MapQuestApp/app/src/main/java/valentecaio/mapquestapp/Point.java
@@ -2,19 +2,52 @@ package valentecaio.mapquestapp;
 
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
+import java.util.ArrayList;
+
 /**
  * Created by caio on 07/05/2017.
  */
 
 public class Point {
+    private String id;
     private double latitude;
     private double longitude;
     private String name;
+    private String description;
+    private ArrayList medias = new ArrayList();
 
     public Point(String name, double latitude, double longitude) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
+    }
+
+    public Point(String id, double latitude, double longitude, String name, String description) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ArrayList getMedias() {
+        return medias;
+    }
+
+    public void addMedia(String path) {
+        this.medias.add(path);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public double getLatitude() {
