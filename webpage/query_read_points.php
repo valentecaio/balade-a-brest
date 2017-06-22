99afd6bd2f7953143f5fdf0144a082f2771756f7
caio valente <caio-valente@hotmail.com>
2017-06-22 17:13:03 +0200

    [web] implements query_read_balades (not tested)

--- webpage/query_read_points.php
+++ webpage/query_read_points.php
@@ -1,6 +1,5 @@
 <?php
    
-    // create example points
     try{
         $bdd = new PDO('mysql:host=localhost;dbname=web18_main;charset=utf8', 'root', '');
     }catch(Exception $e){
@@ -16,33 +15,15 @@
     
     $list = array();
     while ($data = $req->fetch()) {
-        $list[] = array('id' => $data['id_point'], 'name' => $data['nom'], 'lon' => $data['longitude'], 'lat' => $data['latitude'], 'txt' => $data['description']);
-        //echo array('id' => $data['id_point'], 'name' => $data['nom'], 'lon' => $data['longitude'], 'lat' => $data['latitude'], 'txt' => $data['description']);
-        //echo $data['nom'] . ' appartient à ' . $data['id_point'] . '<br />';
+        $list[] = array(
+		'id' => $data['id_point'], 
+		'name' => $data['nom'], 
+		'lon' => $data['longitude'], 
+		'lat' => $data['latitude'], 
+		'txt' => $data['description']);
     }
     $req->closeCursor(); 
-    //echo json_encode($list);
-    //return json_encode($list);
-    echo json_encode($list); //$list;
-    /*
-// simulates loading balades from database
-function get_all_balades() {
-    // create example strolls (balades)
-    var balades = [{
-            name: 'balade1',
-            points: [points[0], points[1], points[2], points[3]]
-        }, {
-            name: 'balade2',
-            points: [points[0], points[1], points[4], points[5]]
-        }, {
-            name: 'balade3',
-            points: [points[4], points[5], points[2], points[3]]
-        }
-    ]
-    return balades;
-}*/
-
-//echo get_aceppted_points();
+    echo json_encode($list);
 
 ?>
 
