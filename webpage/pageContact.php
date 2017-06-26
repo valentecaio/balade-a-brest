088bc55000e77a4c9315c9e6408cd89f89d63e56
eariotti <elnatanariotti@hotmail.com>
2017-06-26 14:58:24 +0200

    [web] title and logo in each page done

--- webpage/pageContact.php
+++ webpage/pageContact.php
@@ -2,11 +2,12 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Contact</title>
+		<title>Balade à Brest</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel='stylesheet' type='text/css' href="style_pageContact.css">
+		<link rel="shortcut icon" type="image/x-icon" href="./images/blue_marker2.png">
 		<script src="lib/jquery.min.js"></script>
 		<script src="lib/bootstrap.min.js"></script>
 	</head>
