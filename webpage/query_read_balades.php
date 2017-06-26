78aad31f1fa5f405622b84efe7bc35582fea7c0b
caio valente <caio-valente@hotmail.com>
2017-06-26 22:27:51 +0200

    [web] query read points return points with medias

--- webpage/query_read_balades.php
+++ webpage/query_read_balades.php
@@ -1,30 +1,4 @@
 <?php
-/*
-    try{
-        $bdd = new PDO('mysql:host=localhost;dbname=web18_main;charset=utf8', 'root', '');
-    }catch(Exception $e){
-        die('Erreur : '.$e->getMessage());
-    }
-
-    if (!isset($_GET['status'])) {
-        $req = $bdd->query('SELECT id_balade, nom, theme, description FROM balade WHERE status = \'accepte\'');
-    } else {
-        $req = $bdd->query('SELECT id_balade, nom, theme, description FROM balade WHERE status = \'en_attente\'');   
-    }
-    
-    $list = array();
-    while ($data = $req->fetch()) {
-        $list[] = array(
-		'id' => $data['id_balade'], 
-		'name' => $data['nom'],
-		'description' => $data['description'],
-		'theme' => $data['theme']);
-    }
-    $req->closeCursor(); 
-    echo json_encode($list);
-
- */  
-    // create example points
     try{
         $bdd = new PDO('mysql:host=localhost;dbname=web18_main;charset=utf8', 'root', '');
     }catch(Exception $e){
@@ -33,8 +7,7 @@
 
     if (!isset($_GET['status'])) {
         $req_balade = $bdd->query('SELECT id_balade, nom, theme, description FROM balade WHERE status = \'accepte\'');
-    }
-    else{
+    } else {
         $req_balade = $bdd->query('SELECT id_balade, nom, theme, description FROM balade WHERE status = \'en_attente\'');   
     }
     
@@ -43,8 +16,7 @@
     while ($data_balade = $req_balade->fetch()) {
 		// get array of points
         $req_points = $bdd->prepare('SELECT id_point, p.nom, latitude, longitude, p.description FROM point as p, balade as b, contenu_parcours as c WHERE b.status = \'accepte\' and p.id_point=c.id_p and b.id_balade=c.id_b and b.id_balade = :id_balade');
-        $req_points->execute(array(
-            'id_balade' => $data_balade['id_balade']));
+        $req_points->execute(array('id_balade' => $data_balade['id_balade']));
 		
 		// fetch array of points
 		$listpoints = array();
@@ -56,9 +28,6 @@
 		// generate new entry to array of balades
         $listbalades[] = array('id' => $data_balade['id_balade'], 'name' => $data_balade['nom'], 'theme' => $data_balade['theme'], 'txt' => $data_balade['description'], 'points' => $listpoints);
     }
-    $req_balade->closeCursor(); 
-    //echo json_encode($list);
-    //return json_encode($list);
-
-    echo json_encode($listbalades); //$list;
+    $req_balade->closeCursor();
+    echo json_encode($listbalades);
 ?>
